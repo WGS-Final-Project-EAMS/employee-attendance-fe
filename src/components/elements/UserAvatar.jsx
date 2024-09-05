b634deaedaebbeb2360f20e9edf2a0babfbf88a6
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-09-05 14:40:40 +0700

    :bug: Fix fetching undefined api problem

--- src/components/elements/UserAvatar.jsx
+++ src/components/elements/UserAvatar.jsx
@@ -2,7 +2,7 @@ import { Avatar } from '@mui/material';
 import { urlEndpoint } from '../../services/url';
 
 const AvatarComponent = ({ url, size = 56 }) => {
-    const avatarUrl = `${urlEndpoint}/${url}`;
+    const avatarUrl = url ? `${urlEndpoint}/${url}` : '';
 
     return (
         <Avatar alt="Profile Picture" sx={{ width: size, height: size }} src={avatarUrl ? avatarUrl : ''} />
